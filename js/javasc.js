5bbb402f42ab9889441949bbdf0d8c3c53b5bf88
pazfrancisco86 <pazfrancisco86@gmail.com>
2021-10-25 22:37:17 -0300

    primeraentrega

---
+++ js/javasc.js
@@ -0,0 +1,10 @@
+/* recibir datos de un usuario*/
+let nombreUsuario = prompt("Ingresa tu nombre!");
+console.log(nombreUsuario);
+let apellidoUsuario = prompt("Ingresa tu apellido!");
+console.log(apellidoUsuario);
+
+/* mostrar un mensaje al usuario*/
+alert("Hola" + " " + nombreUsuario + " " + apellidoUsuario + " " + "bienvenido a tu curso de JavaScript!")
+
+
